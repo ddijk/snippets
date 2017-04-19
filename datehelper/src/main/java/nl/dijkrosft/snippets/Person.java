ac055010b860b590263486d961e4f1c9947f0140
ddijk <dick@dijkrosoft.nl>
2017-04-19 21:41:36 +0200

    Add generics spielerei

---
+++ datehelper/src/main/java/nl/dijkrosft/snippets/Person.java
@@ -0,0 +1,7 @@
+package nl.dijkrosft.snippets;
+
+/**
+ * Created by dickdijk on 19/04/2017.
+ */
+public class Person {
+}
---
+++ datehelper/src/main/java/nl/dijkrosft/snippets/mail/Driver.java
@@ -0,0 +1,9 @@
+package nl.dijkrosft.snippets.mail;
+
+import nl.dijkrosft.snippets.Person;
+
+/**
+ * Created by dickdijk on 19/04/2017.
+ */
+public class Driver extends Person {
+}
